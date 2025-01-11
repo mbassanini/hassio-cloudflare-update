0d75dd5608bc72b84dc9d7fac44b363d5839c73c
mbassanini <79326973+mbassanini@users.noreply.github.com>
2025-01-11 18:32:07 +0100

    Update update_dns_record.py

--- cloudflare_dns_update/update_dns_record.py
+++ cloudflare_dns_update/update_dns_record.py
@@ -22,7 +22,7 @@ def get_Public_IP():
     
 def get_DNS_Record():
     print("INFO: Retrieving DNS records from Cloudlare")
-    connCloudflare.request("GET", "/client/v4/zones/e06f6db8d32b622d0c4af30c81672aec/dns_records", headers=headers)
+    connCloudflare.request("GET", "/client/v4/zones/{zone_ID}/dns_records", headers=headers)
     res = connCloudflare.getresponse()
     response = json.load(res)
     print(response)
